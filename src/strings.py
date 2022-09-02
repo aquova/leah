a1de30a4dbab7c3574945af1eea2373ecde35968
b-b-blueberry <dearbblueberry@gmail.com>
2022-09-02 17:22:54 +1000

    Change failure emoji from a green cross mark to red.

--- src/strings.py
+++ src/strings.py
@@ -10,7 +10,7 @@ with open(file=STRINGS_PATH, mode="r", encoding="utf8") as strings_file:
     _data = json.load(strings_file)
 
 emoji_success = "\N{WHITE HEAVY CHECK MARK}"
-emoji_failure = "\N{NEGATIVE SQUARED CROSS MARK}"
+emoji_failure = "\N{CROSS MARK}"
 
 def get(__name: str):
     return _data.get(__name)
